76adcaa72f2b7d91adc9df55188e5ff5b79287d0
Yoland Gao <yoland5286@gmail.com>
2018-10-22 19:14:57 -0400

    Fix bug

--- upload.py
+++ upload.py
@@ -2,12 +2,12 @@ import Adafruit_DHT as dht
 import time
 
 SENSOR_ID = "test1"
-with open(SENSOR_ID + ".csv", "a+") as f: 
+with open(SENSOR_ID + ".csv", "a+", 0) as f: 
     while True:
         h,t = dht.read(dht.DHT22, 4)
         if h is not None and t is not None:
             print "Temp={0:0.1f}C  Humidity={1:0.1f}%".format(t, h)
             f.write(str(int(time.time())) + "," + str(round(t, 2)) + "," + str(round(h, 2)) + "\n")
-            time.sleep(2.15)
+            time.sleep(3)
         else:
             time.sleep(5)
